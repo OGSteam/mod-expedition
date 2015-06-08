91ed10387c8ed32babbef6e30922b073b5a1bab4
Vodinos <Vodinos@localhost>
2015-06-08 18:23:16 +0200

    Correction du processus d'update

--- update.php
+++ update.php
@@ -7,6 +7,10 @@ if (!defined('IN_SPYOGAME')) {
 
 // We'll need that
 global $db, $table_prefix;
+define("TABLE_EXPEDITION_NUL", $table_prefix."eXpedition_nul"); 		// 0 - Nul
+define("TABLE_EXPEDITION_RESS", $table_prefix."eXpedition_ress"); 		// 1 - Ressources
+define("TABLE_EXPEDITION_FLEET", $table_prefix."eXpedition_fleet"); 	// 2 - Vaisseaux
+define("TABLE_EXPEDITION_MERCH", $table_prefix."eXpedition_merch"); 	// 3 - Marchand
 
 $mod_folder = 'expedition';
 $mod_name   = 'eXpedition';
@@ -16,10 +20,16 @@ $query = "SELECT id, version FROM ".TABLE_MOD." WHERE action='eXpedition'";
 $result = $db->sql_query($query);
 list($mod_id, $version) = $db->sql_fetch_row($result);
 if($version == "1.1.0" || $version == "1.0.2"){
-  $query = "RENAME TABLE ".$table_prefix."expedition_type_0 TO ".$table_prefix."expedition_nul,
-   ".$table_prefix."expedition_type_1 TO ".$table_prefix."expedition_ress,
-   ".$table_prefix."expedition_type_2 TO ".$table_prefix."expedition_fleet,
-   ".$table_prefix."expedition_type_3 TO ".$table_prefix."expedition_merch";
+  $db->sql_query("DROP TABLE IF EXISTS ".TABLE_EXPEDITION_NUL);
+  $db->sql_query("DROP TABLE IF EXISTS ".TABLE_EXPEDITION_RESS);
+  $db->sql_query("DROP TABLE IF EXISTS ".TABLE_EXPEDITION_FLEET);
+  $db->sql_query("DROP TABLE IF EXISTS ".TABLE_EXPEDITION_MERCH);
+
+  $query = "RENAME TABLE 
+   ".$table_prefix."expedition_type_0 TO ".$table_prefix."expedition_nul,
+   ".$table_prefix."expedition_type_1 TO ".$table_prefix."eXpedition_ress,
+   ".$table_prefix."expedition_type_2 TO ".$table_prefix."eXpedition_fleet,
+   ".$table_prefix."expedition_type_3 TO ".$table_prefix."eXpedition_merch";
   $db->sql_query($query);
 
   $query = "DROP TABLE ".$table_prefix."expedition_opts";
