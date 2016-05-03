1051a9eb1ce6e3d7543336a7820b57ac5f4b95ff
Romain Malochet <roms0406@gmx.fr>
2016-05-03 15:37:27 +0200

    Modification des regex (bobsurvivals)

--- _xtense.php
+++ _xtense.php
@@ -8,6 +8,7 @@ if (!defined('IN_SPYOGAME')) {
     exit('Hacking Attempt!');
 }
 
+
 if(class_exists("Callback")){
 class eXpedition_Callback extends Callback {
 	public $version = '2.6.0';
@@ -38,16 +39,29 @@ define("TABLE_EXPEDITION_RESS", $table_prefix."eXpedition_ress"); 		// 1 - Resso
 define("TABLE_EXPEDITION_FLEET", $table_prefix."eXpedition_fleet"); 	// 2 - Vaisseaux
 define("TABLE_EXPEDITION_MERCH", $table_prefix."eXpedition_merch"); 	// 3 - Marchand
 
-
+function logging($msg)
+{
+	$dir = date('ymd');
+	$file = 'log_expedition.log';
+	if (!file_exists('journal/'.$dir)) @mkdir('journal/'.$dir);
+	if (file_exists('journal/'.$dir)) {
+		@chmod('journal/'.$dir, 0777);
+		$fp = @fopen('journal/'.$dir.'/'.$file, 'a+');
+		if ($fp) {
+			fwrite($fp, date('d/m/Y H:i:s').' - '.$msg."\r\n");
+			fclose($fp);
+			@chmod('journal/'.$dir.'/'.$file, 0777);
+		}
+	}
+}
 function eXpedition_analysis($uid, $galaxy, $system, $timestamp, $content)
 {
 	global $db;
-
 	// REGEX
-	$regexFleet = "#Votre\sflotte\ss`est\sagrandie,\svoici\sles\snouveaux\svaisseaux\squi\ss`y\ssont\sjoints\s:(.+)#";
-	$regexRess  = "#L`attaquant\sobtient\s(\S+)\s(\d+(.?\d*)?)#";
-	$regexMerch = ["#liste\sde\sclients\sprivilégiés#", "#dans\svotre\sempire\sun\sreprésentant\schargé\sde\sressources\sà\séchanger#"];
-	
+ 	$regexFleet = '/Votre\sflotte\ss\`est\sagrandie\,\svoici\sles\snouveaux\svaisseaux\squi\ss\`y\ssont\sjoints\s\:(.+\d)/';
+ 	$regexRess  = "/L`attaquant\sobtient\s(\S+)\s[\(\AM\)]*\s?([\d+\.+]*)/";
+ 	$regexMerch = ["liste\sde\sclients\sprivilégiés", "dans\svotre\sempire\sun\sreprésentant\schargé\sde\sressources\sà\séchanger"];
+
 	// Check if new
 	$query = "SELECT * 
 		FROM ".TABLE_EXPEDITION." 
@@ -55,61 +69,65 @@ function eXpedition_analysis($uid, $galaxy, $system, $timestamp, $content)
 		AND date = $timestamp 
 		AND pos_galaxie = $galaxy 
 		AND pos_sys = $system";
+	logging("Content : ".$content);
 	if($db->sql_numrows($db->sql_query($query)) == 0)
 	{
+		logging("FLEET ?");
 		//FLEET ?
 		if(preg_match($regexFleet, $content, $expFleet) != 0)
 		{
+			logging("Flotte0".$expFleet[0]);
+			logging("Flotte1".$expFleet[1]);
 			// Init
 			$pt = $gt = $cle = $clo = $cr = $vb = $vc = $rec = $se = $bmb = $dst = $tra = 0;
 			$units = 0;
 			
 			// Looking for specifics
-			if(preg_match("#Petit\stransporteur\s(\d+)#", $expFleet[1], $reg)){
+			if(preg_match("/Petit\stransporteur:\s(\d+)/", $expFleet[1], $reg)){
 				$pt = $reg[1]; 
 				$units += ( 2  + 2  + 0  ) * $pt;
 			}
-			if(preg_match("#Grand\stransporteur\s(\d+)#", $expFleet[1], $reg)){
+			if(preg_match("/Grand\stransporteur:\s(\d+)/", $expFleet[1], $reg)){
 				$gt = $reg[1]; 
 				$units += ( 6  + 6  + 0  ) * $gt;
 			}
-			if(preg_match("#Chasseur\sléger\s(\d+)#", $expFleet[1], $reg)){
+			if(preg_match("/Chasseur\sléger:\s(\d+)/", $expFleet[1], $reg)){
 				$cle = $reg[1]; 
 				$units += ( 3  + 1  + 0  ) * $cle;
 			}
-			if(preg_match("#Chasseur\slourd\s(\d+)#", $expFleet[1], $reg)){
+			if(preg_match("/Chasseur\slourd:\s(\d+)/", $expFleet[1], $reg)){
 				$clo = $reg[1]; 
 				$units += ( 6  + 4  + 0  ) * $clo;
 			}
-			if(preg_match("#Croiseur\s(\d+)#", $expFleet[1], $reg)){
+			if(preg_match("/Croiseur:\s(\d+)/", $expFleet[1], $reg)){
 				$cr = $reg[1]; 
 				$units += ( 20 + 7  + 2  ) * $cr;
 			}
-			if(preg_match("#Vaisseau\sde\sbataille\s(\d+)#", $expFleet[1], $reg)){
+			if(preg_match("/Vaisseau\sde\sbataille:\s(\d+)/", $expFleet[1], $reg)){
 				$vb = $reg[1]; 
 				$units += ( 45 + 15 + 0  ) * $vb;
 			}
-			if(preg_match("#Vaisseau\sde\scolonisation\s(\d+)#", $expFleet[1], $reg)){
+			if(preg_match("/Vaisseau\sde\scolonisation:\s(\d+)/", $expFleet[1], $reg)){
 				$vc = $reg[1]; 
 				$units += ( 10 + 20 + 10 ) * $vc;
 			}
-			if(preg_match("#Recycleur\s(\d+)#", $expFleet[1], $reg)){
+			if(preg_match("/Recycleur:\s(\d+)/", $expFleet[1], $reg)){
 				$rec = $reg[1]; 
 				$units += ( 10 + 6  + 2  ) * $rec;
 			}
-			if(preg_match("#Sonde\sd`espionnage\s(\d+)#", $expFleet[1], $reg)){
+			if(preg_match("/Sonde\sd`espionnage:\s(\d+)/", $expFleet[1], $reg)){
 				$se = $reg[1]; 
 				$units += ( 0  + 1  + 0  ) * $se;
 			}
-			if(preg_match("#Bombardier\s(\d+)#", $expFleet[1], $reg)){
+			if(preg_match("/Bombardier:\s(\d+)/", $expFleet[1], $reg)){
 				$bmb = $reg[1]; 
 				$units += ( 50 + 25 + 15 ) * $bmb;
 			}
-			if(preg_match("#Destructeur\s(\d+)#", $expFleet[1], $reg)){
+			if(preg_match("/Destructeur:\s(\d+)/", $expFleet[1], $reg)){
 				$dst = $reg[1]; 
 				$units += ( 60 + 50 + 15 ) * $dst;
 			}
-			if(preg_match("#Traqueur\s(\d+)#", $expFleet[1], $reg)){
+			if(preg_match("/Traqueur:\s(\d+)/", $expFleet[1], $reg)){
 				$tra = $reg[1]; 
 				$units += ( 30 + 40 + 15 ) * $tra;
 			}
@@ -131,8 +149,12 @@ function eXpedition_analysis($uid, $galaxy, $system, $timestamp, $content)
 		}
 		// RESSOURCES ?
 		else if(preg_match($regexRess, $content, $expRess) != 0){
+			logging("RESSOURCES");
+			logging("Ressources1 : ".$expRess[1]);
+			logging("Ressources2 : ".$expRess[2]);
+			//	L`attaquant obtient Antimatière (AM) 310.Extrait du journal
 			log_('debug', $expRess[2]);
-			if(preg_match("#Métal#", $expRess[1]))
+			if(preg_match("/Métal/", $expRess[1]))
 			{
 				$typeRess = 0;
 				$met = str_replace('.','', $expRess[2]);
@@ -140,7 +162,7 @@ function eXpedition_analysis($uid, $galaxy, $system, $timestamp, $content)
 				$deut = 0;
 				$antimat = 0;
 			}
-			if(preg_match("#Cristal#", $expRess[1]))
+			if(preg_match("/Cristal/", $expRess[1]))
 			{
 				$typeRess = 1;
 				$met = 0;
@@ -148,7 +170,7 @@ function eXpedition_analysis($uid, $galaxy, $system, $timestamp, $content)
 				$deut = 0;
 				$antimat = 0;
 			}
-			if(preg_match("#Deutérium#", $expRess[1]))
+			if(preg_match("/Deutérium/", $expRess[1]))
 			{
 				$typeRess = 2;
 				$met = 0;
@@ -156,7 +178,7 @@ function eXpedition_analysis($uid, $galaxy, $system, $timestamp, $content)
 				$deut = str_replace('.','', $expRess[2]);
 				$antimat = 0;
 			}
-			if(preg_match("#Antimatière#", $expRess[1]))
+			if(preg_match("/Antimatière/", $expRess[1]))
 			{
 				$typeRess = 3;
 				$met = 0;
@@ -169,23 +191,26 @@ function eXpedition_analysis($uid, $galaxy, $system, $timestamp, $content)
 				die("Parsing Error");
 			}
 			
+			logging("Ressources : Insertion table : ".TABLE_EXPEDITION);
 			$query = 
 					"INSERT INTO ".TABLE_EXPEDITION." 
 					(id, user_id, date, pos_galaxie, pos_sys, type) 
 					VALUES ('', $uid, $timestamp, $galaxy, $system, 1)";
 			$db->sql_query($query);
 			$idInsert = $db->sql_insertid();
-			
+			logging("Ressources : Insertion table : ".TABLE_EXPEDITION." = OK");
+			logging("Ressources : Insertion table : ".TABLE_EXPEDITION_RESS);
 			$query = 
 				"INSERT INTO ".TABLE_EXPEDITION_RESS." 
 				(id, id_eXpedition, typeRessource, metal, cristal, deuterium, antimatiere) 
 				VALUES ('', $idInsert, $typeRess, $met, $cri, $deut, $antimat)";
 			$db->sql_query($query);
-			
+			logging("Ressources : Insertion table : ".TABLE_EXPEDITION_RESS." = OK");
 			return true;
 		}
 		// MERCHANT ?
 		else if( (preg_match($regexMerch[0], $content) != 0) || (preg_match($regexMerch[1], $content) != 0) ){
+			logging("Marchand");
 			$query = 
 				"INSERT INTO ".TABLE_EXPEDITION." 
 				(id, user_id, date, pos_galaxie, pos_sys, type) 
@@ -202,6 +227,7 @@ function eXpedition_analysis($uid, $galaxy, $system, $timestamp, $content)
 		}
 		// OTHER
 		else{
+			logging("Autre");
 			$query = 
 				"INSERT INTO ".TABLE_EXPEDITION." 
 				(id, user_id, date, pos_galaxie, pos_sys, type) 
@@ -216,6 +242,8 @@ function eXpedition_analysis($uid, $galaxy, $system, $timestamp, $content)
 			
 			return true;
 		}
+		
+		logging("FIN");
 	}
 	else {
 		return false;	
